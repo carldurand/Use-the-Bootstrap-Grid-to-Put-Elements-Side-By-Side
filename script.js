3e44922a3d6642f8a3bf5c4a5415d24b82ba188a
Carl Durand <48800487+carldurand@users.noreply.github.com>
2019-05-17 21:52:08 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function getUserInfo(){
-    const input = document.getElementById("userName").value;
-    
-    axios.get("/showprofile/" + input)
-    .then(response => {
-        console.log("axios", response.data);
-    })
-}
